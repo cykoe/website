aba7efd0a3946430edb5809b97bb28da92257c52
sircharlie <esculin.hao@gmail.com>
2020-01-16 10:55:33 -0600

    working on responsive

--- src/App.tsx
+++ src/App.tsx
@@ -28,127 +28,106 @@ import {
 
 const App: React.FC = () => {
   return (
-      <div className='root'>
-        <div className="app">
-          <div className='main'>
-            <div className='row'>
-              <div className='col-1'/>
-              <div className="col-1">
-                <img src={Logo} alt="logo"/>
-              </div>
+      <div className="App">
+        <div className='main-section'>
+          <div className='row'>
+            <div className='col-1 col-xl-2'/>
+            <div className="col-1">
+              <img src={Logo} alt="logo"/>
             </div>
-            <div className='row introduction'>
-              <div className='col-1'/>
-              <div className='col-3 vertical-align--bottom'>
-                <ul className="float-left">
-                  <li><img src={weibo_dark} alt="weibo"/></li>
-                  <li><img src={twitter_dark} alt="twitter"/></li>
-                  <li><img src={youtube_dark} alt="youtube"/></li>
-                  <li><img src={email_dark} alt="email"/></li>
+          </div>
+          <div className='row introduction'>
+            {/*TODO: restore this*/}
+            {/*<div className='col-1 col-xl-2 col-xs-1'/>*/}
+            <div className='col-2 col-l-3 col-xl-1 col-xs-2 vertical-align--bottom introduction__logos'>
+              <ul>
+                <li><img src={weibo_dark} alt="weibo"/></li>
+                <li><img src={twitter_dark} alt="twitter"/></li>
+                <li><img src={youtube_dark} alt="youtube"/></li>
+                <li><img src={email_dark} alt="email"/></li>
+              </ul>
+            </div>
+            <div className='col-3 col-l-2 col-xl-3 col-xs-6 vertical-align--center text-center introduction__titles'>
+              <p className='font-2--0em'>Charlie Yang</p>
+              <p className='font-1--3em'>Software Engineer</p>
+            </div>
+            <div className='col-4 col-xl-3 col-xs-6 vertical-align--center introduction__logo'>
+              <img src={MainLogo} alt=""/>
+            </div>
+            <div className='col-2 col-xs-2 vertical-align--bottom text-center introduction__scroll'>
+              <span className='scroll-down-text'>SCROLL DOWN</span>
+              <img src={vertial_line} alt="line"/>
+            </div>
+          </div>
+        </div>
+
+        <div className='list-section'>
+          <div className='row list-section__item'>
+            <div className='col-2 col-xl-3'/>
+            <div className='col-9 col-xl-7 project'>
+              <div className='col-4 col-l-5 project__images'>
+                <ul>
+                  <li><img src={image} alt=""/></li>
+                  <li><img src={image} alt=""/></li>
                 </ul>
               </div>
-              <div
-                  className='col-2 vertical-align--center introduction__titles'>
-                <p className='font-2--5em'>Charlie Yang</p>
-                <p className='font-1--3em'>Software Engineer</p>
-              </div>
-              <div className='col-3 vertical-align--center introduction__logo'>
-                <img src={MainLogo} alt=""/>
-              </div>
-              <div
-                  className='col-3 vertical-align--bottom introduction__scroll'>
-                <span className='scroll-down-text'>SCROLL DOWN</span>
-                <img src={vertial_line} alt="line"/>
-              </div>
-            </div>
-          </div>
-
-          <div className='project-list'>
-            <div className='row project-list'>
-              <div className='col-1'></div>
-              <div className='project-list-item col-10'>
-                <div className='col-5 project-list-item-images'>
-                  <ul>
-                    <li><a href='/projects'><img src={image}/></a></li>
-                    <li><img src={image}/></li>
-                  </ul>
-                </div>
-                {/* images */}
-                <div className='col-5 project-list-item-name'>
-                  <div className='item-name-section'>
-                    <div className='font-3em'><img src={horizontal_line}/></div>
-                    <div className='font-3em'>01</div>
-                    <div className='title font-3em'>
-                      Zblit
-                      <div className='sub-title font-2em'>next-generataion
-                        receipt
-                        splitting
-                        platform
-                      </div>
-                    </div>
+              <div className='col-8 col-l-5 project__title'>
+                <div className='item-name-section'>
+                  <div className='font-2--0em'><p>01</p></div>
+                  <div>
+                    <p className='font-2--0em'>Zblit</p>
+                    <p className='font-1--1em'>next-generation receipt splitting
+                      platform</p>
                   </div>
                 </div>
-                {/* title */}
-                <div className='col-4'></div>
-                {/* buffer zone*/}
-                <div
-                    className='col-3 project-list-item-view vertical-align--center'>
-                  <img src={view_project}/>
-                </div>
-                {/* view-project button */}
               </div>
-              <div className='col-1'></div>
+              <div className='col-1 col-l-4'/>
+              <div className='col-3 col-l-3 project__view vertical-align--center'>
+                <img src={view_project} alt=""/>
+              </div>
+            </div>
+            <div className='col-1 col-xl-2'/>
             </div>
 
-            <div className='row project-list'>
-              <div className='col-1'></div>
-              <div className='project-list-item-reverse col-10'>
-                <div
-                    className='col-3 project-list-item-view vertical-align--center'>
-                  <img src={view_project}/>
-                </div>
-                {/* view-project button */}
-                <div className='col-2 project-list-item-space'></div>
-                {/* buffer zone*/}
-                <div className='col-5 project-list-item-name'>
-                  <div className='item-name-section'>
-                    <div className='font-3em'><img src={horizontal_line}/></div>
-                    <div className='font-3em'>01</div>
-                    <div className='title font-3em'>
-                      Zblit
-                      <div className='sub-title font-2em'>next-generataion
-                        receipt
-                        splitting
-                        platform
-                      </div>
-                    </div>
+          <div className='row list-section__item'>
+            <div className='col-1 col-xl-2'/>
+            <div className='col-9 col-xl-7 project--reverse'>
+              <div className='col-3 col-l-3 project__view vertical-align--center'>
+                <img src={view_project} alt=""/>
+              </div>
+              <div className='col-1 col-l-4 project__buffer'/>
+              <div className='col-8 col-l-5 project__title'>
+                <div className='item-name-section'>
+                  <div className='font-2--5em'>01</div>
+                  <div>
+                    <p className='font-2--0em'>Zblit</p>
+                    <p className='font-1--1em'>next-generation receipt splitting
+                      platform</p>
                   </div>
                 </div>
-                {/* title */}
-                <div className='col-5 project-list-item-images'>
-                  <ul>
-                    <li><img src={image}/></li>
-                    <li><img src={image}/></li>
-                  </ul>
-                </div>
-                {/* images */}
               </div>
-              <div className='col-1'></div>
+              <div className='col-4 col-l-5 project__images'>
+                <ul>
+                  <li><img src={image} alt=""/></li>
+                  <li><img src={image} alt=""/></li>
+                </ul>
+              </div>
             </div>
+            <div className='col-2 col-xl-3'/>
+          </div>
+        </div>
+
+        <div className='footer-section text-center'>
+          <ul className="col-12">
+            <li><img src={weibo_light} alt=""/></li>
+            <li><img src={twitter_light} alt=""/></li>
+            <li><img src={youtube_light} alt=""/></li>
+            <li><img src={email_light} alt=""/></li>
+          </ul>
+          <div>
+            <img src={copyright} alt=""/>
 
           </div>
-
-          {/*<div className='row footer'>*/}
-          {/*  <ul className="float-left col-12 text-center">*/}
-          {/*    <li><img src={weibo_light}/></li>*/}
-          {/*    <li><img src={twitter_light}/></li>*/}
-          {/*    <li><img src={youtube_light}/></li>*/}
-          {/*    <li><img src={email_light}/></li>*/}
-          {/*  </ul>*/}
-          {/*  <div className='footer-copyright text-center'>*/}
-          {/*    <img src={copyright}/>*/}
-          {/*  </div>*/}
-          {/*</div>*/}
         </div>
       </div>
   );
